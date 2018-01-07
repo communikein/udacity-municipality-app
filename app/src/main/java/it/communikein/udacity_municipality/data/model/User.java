92612a9b5bbcb545d8e4cb146bb0afae61dacfd7
Francesco Reale <francescoa.reale@gmail.com>
2018-01-07 23:39:04 +0100

    inserting data user in realtime database

---
+++ app/src/main/java/it/communikein/udacity_municipality/data/model/User.java
@@ -0,0 +1,34 @@
+package it.communikein.udacity_municipality.models;
+
+//import com.google.firebase.
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+
+
+// [START user_class]
+@IgnoreExtraProperties
+public class User {
+    public enum typeOfUser {
+
+        Citizien,
+        Municipality_Worker
+
+    };
+
+    public String username;
+    public String email;
+    public typeOfUser type;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String email,typeOfUser type) {
+        this.username = username;
+        this.email = email;
+        this.type = type;
+    }
+
+}
+// [END blog_user_class]
